7138d6756f70a3df09f3c8580d275c8a3978def8
jithin <jeets2689@gmail.com>
2020-04-13 00:16:33 +0530

    add row feature added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,33 +1,49 @@
 class ProductsController < ApplicationController
 	protect_from_forgery except: :edit
 
-  def index
-  	Product.read_csv
-  	@products = Product.all
-  end
+    def index
+    	Product.read_csv
+    	@products = Product.all
+    end
 
 
-  def edit
-  	# binding.pry
-    @product = Product.find(params[:id])
-    @index = params["format"].to_i
+    def add_row
+      @products = Product.all
+      @product = Product.new
+    end
+
+    def create
+     
+      @product = Product.create(product_name: params["product"]["product_name"],product_descprition: params["product"]["product_description"],brand_name: params["product"]["brand_name"] )
+       Product.write_csv
+      redirect_to products_path
     
-  end
 
-  def save_product
-    # debugger
-    @product = Product.find(params["product_id"].keys.first)
-    @product.update(product_name: params["product"]["product_name"],product_descprition: params["product"]["product_descprition"],brand_name: params["product"]["brand_name"] )
-    Product.write_csv
-    redirect_to products_path
-   
-  end
+    end
 
+    def edit
+    	# binding.pry
+      @product = Product.find(params[:id])
+      @index = params["format"].to_i
+      
+    end
 
-  def destroy 
-    @product = Product.find(params[:id])
-    @product.destroy
-    Product.write_csv
-    redirect_to products_path
-  end
+    def save_product
+      # debugger
+      @product = Product.find(params["product_id"].keys.first)
+      @product.update(product_name: params["product"]["product_name"],product_descprition: params["product"]["product_descprition"],brand_name: params["product"]["brand_name"] )
+      Product.write_csv
+      redirect_to products_path
+     
+    end
+
+    def destroy 
+      @product = Product.find(params[:id])
+      @product.destroy
+      Product.write_csv
+      redirect_to products_path
+    end
+
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'save_product', to: 'products#save_product'
   post 'save_product', to: 'products#save_product'
+  get 'add_row', to: 'products#add_row'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
